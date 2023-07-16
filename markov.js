57853735b9d88c995f4a1fa3e4218b91c609b217
Ryan Broome <ryan@MacBook-Pro.local>
2023-07-16 17:52:25 -0400

    remove notes.js prior to submit

--- markov.js
+++ markov.js
@@ -1,29 +1,35 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
+    this.chains = {};
     this.makeChains();
+    // this.makeChains();
   }
 
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+  //    *  for text of "the cat in the hat", chains will be
+  //    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    const words = this.words;
+    const chains = this.chains;
+    for (let i = 0; i < words.length; i++) {
+      chains[words[i]] = words.filter(function (value, index) {
+        return words[i] === words[index - 1];
+      });
+    }
+    this.chains = chains;
+    return chains;
   }
 
-
-  /** return random text from chains */
-
   makeText(numWords = 100) {
-    // TODO
+    const chains = this.chains;
+    let index = Math.floor(Math.random() * this.words.length);
+    let keys = Object.keys(chains);
+    console.log(keys);
   }
 }
